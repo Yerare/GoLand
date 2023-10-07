1369c1fca9bc6e5ada69bf1eb64fe9e67d672e04
Yerzhigit <yerjigitzhakhan@gmail.com>
2023-10-07 21:17:38 +0600

    4.com

--- easymath/cmd/api/formulas.go
+++ easymath/cmd/api/formulas.go
@@ -2,13 +2,51 @@ package main
 
 import (
 	"easymath/internal/data" // New import
+	"easymath/internal/validator"
 	"fmt"
 	"net/http"
 	"time" // New import
 )
 
 func (app *application) createformulaHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new formula")
+	var input struct {
+		Chapter string       `json:"chapter"`
+		Level   string       `json:"levels"`
+		Withvar data.Withvar `json:"count"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	formulas := &data.Formulas{
+		Chapter: input.Chapter,
+		Withvar: input.Withvar,
+		Level:   input.Level,
+	}
+	// Initialize a new Validator instance.
+	v := validator.New()
+	// Use the Check() method to execute our validation checks. This will add the
+	// provided key and error message to the errors map if the check does not evaluate
+	// to true. For example, in the first line here we "check that the title is not
+	// equal to the empty string". In the second, we "check that the length of the title
+	// is less than or equal to 500 bytes" and so on.
+	v.Check(input.Chapter != "", "chapter", "must be provided")
+	v.Check(len(input.Chapter) <= 500, "chapter", "must not be more than 500 bytes long")
+	v.Check(input.Withvar != 0, "runtime", "must be provided")
+	v.Check(input.Withvar > 0, "runtime", "must be a positive integer")
+	v.Check(input.Level != "", "chapter", "must be provided")
+	v.Check(len(input.Level) <= 500, "levels", "must not be more than 500 bytes long")
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
+	if data.ValidateFormulas(v, formulas); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showformulaHandler(w http.ResponseWriter, r *http.Request) {
--- easymath/internal/data/formulas.go
+++ easymath/internal/data/formulas.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"easymath/internal/validator"
 	"time"
 )
 
@@ -10,4 +11,14 @@ type Formulas struct {
 	Chapter   string    `json:"chapter"`
 	Level     string    `json:"levels,omitempty"`
 	Withvar   Withvar   `json:"count"`
+}
+
+func ValidateFormulas(v *validator.Validator, formula *Formulas) {
+	v.Check(formula.Chapter != "", "chapter", "must be provided")
+	v.Check(len(formula.Chapter) <= 500, "chapter", "must not be more than 500 bytes long")
+	v.Check(formula.Withvar != 0, "count", "must be provided")
+	v.Check(formula.Withvar > 0, "count", "must be a positive integer")
+	v.Check(formula.Level != "", "levels", "must be provided")
+	v.Check(len(formula.Level) <= 500, "levels", "must not be more than 500 bytes long")
+
 }
